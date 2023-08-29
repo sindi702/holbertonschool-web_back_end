315585a1a430ffc3c6a4275d5a79d08b9ea68848
sindi702 <hamatajsindi@gmail.com>
2023-08-29 01:52:56 -0700

    test

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+'''documentation'''
+
+
+def concat(str1: str, str2: str) -> str:
+    '''fun that concat 2 strings'''
+    return str1+str2
---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''documentation'''
+
+import math
+
+
+def floor(n: float) -> int:
+    '''floor func'''
+    floor_x = math.floor(n)
+    return (floor_x)
